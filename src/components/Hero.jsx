ed65e673cd14f6113eb5cab17fcd5700f284d34f
Kendrick Bong <kendrickbong1996@gmail.com>
2023-03-16 18:46:06 +0800

    textloop done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,26 @@
-import { motion } from 'framer-motion'
-
+import React from 'react'
 import { styles } from '../styles'
+import { motion } from 'framer-motion'
 import { ComputersCanvas } from './canvas'
+import TextTransition, { presets } from 'react-text-transition'
+
+const TEXTS = [
+    'Coffee Addict',
+    'Software Engineer',
+    'Swing Trader',
+    '"Comedian"',
+]
 
 const Hero = () => {
+    const [index, setIndex] = React.useState(0)
+
+    React.useEffect(() => {
+        const intervalId = setInterval(
+            () => setIndex((index) => index + 1),
+            2000
+        )
+        return () => clearTimeout(intervalId)
+    }, [])
     return (
         <section className="relative w-full h-screen mx-auto">
             <div
@@ -15,11 +32,23 @@ const Hero = () => {
                 </div>
                 <div>
                     <h1 className={`${styles.heroHeadText} text-white`}>
-                        Hi, I'm <span className="text-[#829edd]">Kendrick</span>
+                        Hi, I'm Kendrick
                     </h1>
-                    <h1></h1>
+                    <h1
+                        className={`font-black text-[#829edd] lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2`}
+                    >
+                        <TextTransition springConfig={presets.stiff}>
+                            {TEXTS[index % TEXTS.length]}
+                        </TextTransition>
+                    </h1>
+                    <h2
+                        className={`font-black text-white lg:text-[40px] sm:text-[30px] xs:text-[25px] text-[25px] lg:leading-[98px] my-4`}
+                    >
+                        based in Perth, Australia
+                    </h2>
                 </div>
             </div>
+            <ComputersCanvas />
         </section>
     )
 }
