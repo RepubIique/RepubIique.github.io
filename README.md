f289b636577e4bed036bbf1cf7904eb89940c8ed
Kendrick Bong <kendrickbong1996@gmail.com>
2023-03-16 19:19:15 +0800

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# three-portfolio
-New portfolio incoming
--- src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-declare module 'three'
-declare module 'three/examples/*'
-declare module './components/*'
-declare module 'react-text-loop-next'
